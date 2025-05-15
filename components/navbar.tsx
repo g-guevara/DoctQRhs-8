8f690f8c971861fdf9c1f0403451c82d5c7da534
gguevara112 <guevara.guillermo.g@gmail.com>
2025-05-15 16:11:30 -0400

    no darkkmode button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -178,7 +178,7 @@ export const Navbar = () => {
             <DropdownItem key="FR">Français</DropdownItem>
           </DropdownMenu>
         </Dropdown>
-        <ThemeSwitch />
+  
         <NavbarMenuToggle />
       </NavbarContent>
 
